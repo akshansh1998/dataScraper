1590d8696e0385441091fb571c5772d229b4712e
Ankush Anand <ankushanand718@gmail.com>
2023-01-27 11:25:14 +0530

    Beta Version

---
+++ README.md
@@ -0,0 +1,2 @@
+# dataScraper
+ Scrape Data from website using Python, Script Accept an 3 Inputs 1 URL, 1 CSS Selector for links to select on first page and 1 CSS Selector for scrapping data from next page
---
+++ dataScraper.py
@@ -0,0 +1,127 @@
+# Import Modules
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+from time import sleep
+from random import randint
+import validators
+from cssselect.parser import parse
+
+
+# Defining Some functions
+# ------------------------------------------------------------------------------
+# CSS Validator
+def validate_css_selector(input_text):
+    while True:
+        selector = input(input_text)
+        try:
+            parse(selector)
+            print("Your CSS selector looks good.")
+            return selector
+        except Exception:
+            print("This is not a valid CSS selector. Please try again.")
+
+# Ask for URL
+while True:
+    url = input("Submit the URL: ")
+    if validators.url(url):
+        print("That sounds like a URL...")
+        break
+    
+    else:
+        print("\nThat doesn't seems like a URL, please try again.\n") 
+
+# Ask for CSS Selector
+selector = validate_css_selector("Enter the CSS selector of Parent Page: ")
+
+
+print(f"URL is {url}")
+print(f"CSS Selector is {selector}")
+
+# Check and store all instances in a list
+
+print ("Holay! Shit just started...")
+web = webdriver.Chrome()
+print("Launched chrome")
+
+web.get(url)
+print("Waiting..")
+input("Press Enter to continue...")
+
+# Get the scroll height of the div
+div_scroll_height = web.execute_script("return document.getElementsByClassName('_vertical-scroll-results_1igybl')[0].scrollHeight;")
+pixels = 200
+while True:
+    # Scroll down the div by the set number of pixels
+    web.execute_script("document.getElementsByClassName('_vertical-scroll-results_1igybl')[0].scrollBy(0,arguments[0]);", pixels)
+    time.sleep(3)
+    # Get the new scroll height of the div
+    new_div_scroll_height = web.execute_script("return document.getElementsByClassName('_vertical-scroll-results_1igybl')[0].scrollHeight;")
+    # Check if the scroll height of the div has changed
+    if new_div_scroll_height == div_scroll_height:
+        break
+    div_scroll_height = new_div_scroll_height
+time.sleep(5)
+print("Checking for elements..")
+
+
+
+elements = web.find_elements(By.CSS_SELECTOR, selector)
+
+
+# Print the number of elements found
+print(f"Number of elements found: {len(elements)}")
+
+
+try:
+    # Find the element containing the desired information
+    child_selector = validate_css_selector("Enter the CSS selector of Child Page: ")
+
+    # Iterate through the list of elements
+    for element in elements:
+        print(element.text)
+
+        # Get the link's URL from the element
+        link = element.get_attribute("href")
+        sleep(randint(5,10))
+
+        # Open the link in a new tab
+        actions = ActionChains(web)
+        actions.key_down(Keys.CONTROL).click(element).key_up(Keys.CONTROL)
+        actions.perform()
+
+        # Switch to the new tab
+        web.switch_to.window(web.window_handles[-1])
+
+        # Wait for page to load
+        try:
+            element = WebDriverWait(web, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, child_selector))
+            )
+            # Copy the text from the desired element
+            copied_text = element.get_attribute("href")
+
+            # Print the copied text
+            print(copied_text)
+
+        except Exception as e:
+            print(f'Error: {e}')
+
+        # Close the new tab
+        web.close()
+
+        # Switch back to the original tab
+        web.switch_to.window(web.window_handles[0])
+    # Close the webdriver
+    # web.quit()
+
+    print("All Done! Exiting")
+except KeyboardInterrupt:
+    # web.quit()
+    print("Exiting")
+    # exit()
